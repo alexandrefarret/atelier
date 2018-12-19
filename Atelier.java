9fbb1a760046617ba037564b9439aff7f18e6b34
hugocrozes <46008435+hugocrozes@users.noreply.github.com>
2018-12-19 22:21:13 +0100

    Add files via upload

--- Atelier.java
+++ Atelier.java
@@ -4,75 +4,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Atelier {
-	protected String atelier;
+	protected String sAtelier;
 	protected List<Ilot> lesIlots;
-	protected int[][] tempsTransfertParcelle;
-	protected int nombreParcelles;
+	protected int[][] iTTempsTransfertParcelle;
+	protected int iNombreParcelles;
 	
 	/**
 	 * Exchange island places 
 	 * @param unIlot The island 
-	 * @param uneParcelle A parcel
+	 * @param iUneParcelle A parcel
 	 */
-	public void deplacerIlot(Ilot unIlot, int uneParcelle){
-		boolean testExiste = false;
+	public void deplacerIlot(Ilot unIlot, int iUneParcelle){
+		boolean bTestExiste = false;
 		Ilot ilotTemp = new Ilot(0, null,0);
 		// Foreach island, check if parcel is use
 		// Parcel use = switch the parcels
 		// Not used parcel = the island got the parcel
 		for(Ilot ilotVerif : lesIlots)
 		{
-			if(ilotVerif.getParcelle() == uneParcelle)
+			if(ilotVerif.getParcelle() == iUneParcelle)
 			{
-				testExiste = true;
-				ilotVerif.setParcelle(unIlot.parcelle);
-				unIlot.setParcelle(uneParcelle);
+				bTestExiste = true;
+				ilotVerif.setParcelle(unIlot.iParcelle);
+				unIlot.setParcelle(iUneParcelle);
 			}
 		}
-		if(!testExiste)
+		if(!bTestExiste)
 		{
-			unIlot.parcelle = uneParcelle;
+			unIlot.iParcelle = iUneParcelle;
 		}
 	}
 	
 	
 	/**
 	 * Create time between parcels
-	 * @param nombreParcelles Parcel number
+	 * @param iNombreParcelles Parcel number
 	 */
-	public void creationTempsParcelle(int nombreParcelles){
+	public void creationTempsParcelle(int iNombreParcelles){
 		int n;
 		//Creation of the number of parcels
-		tempsTransfertParcelle = new int[nombreParcelles][nombreParcelles];
+		iTTempsTransfertParcelle = new int[iNombreParcelles][iNombreParcelles];
 		//Browse the first dimension of the board
-		for(int i=0; i<nombreParcelles; i++)
+		for(int i=0; i<iNombreParcelles; i++)
 		{
-			n = nombreParcelles-1;
+			n = iNombreParcelles-1;
 			//Generates a random number between 0 and 20
 			int aleatoire = (int)(Math.random() * 20);
 			//Browse the second dimension of the board
 			while(n != 0)
 			{
-				tempsTransfertParcelle[i][n] = aleatoire;
-				tempsTransfertParcelle[n][i] = aleatoire;
+				iTTempsTransfertParcelle[i][n] = aleatoire;
+				iTTempsTransfertParcelle[n][i] = aleatoire;
 				aleatoire = (int)(Math.random() * 20);
 				n = --n;				
 			}
 		//Puts 0 the time of the parcels to itself
-		tempsTransfertParcelle[i][i] = 0;
+		iTTempsTransfertParcelle[i][i] = 0;
 		}
 	}
 	
 	/**
 	 * Constructor
-	 * @param atelier The name of atelier
-	 * @param nombreParcelles Quantity of parcel
+	 * @param sAtelier The name of atelier
+	 * @param iNombreParcelles Quantity of parcel
 	 */
-	public Atelier(String atelier, int nombreParcelles) {
-		this.atelier = atelier;
+	public Atelier(String sAtelier, int iNombreParcelles) {
+		this.sAtelier = sAtelier;
 		this.lesIlots = new ArrayList<Ilot>();
-		creationTempsParcelle(nombreParcelles);
-		this.nombreParcelles = nombreParcelles;
+		creationTempsParcelle(iNombreParcelles);
+		this.iNombreParcelles = iNombreParcelles;
 	}
 	
 	/**
@@ -82,24 +82,24 @@ public class Atelier {
 	 */
 	public void afficher(int i, int n)
 	{
-		System.out.println(tempsTransfertParcelle[i][n]);
+		System.out.println(iTTempsTransfertParcelle[i][n]);
 	}
 	
 	// Getters/Setters
 	public String getAtelier() {
-		return atelier;
+		return sAtelier;
 	}
 
-	public void setAtelier(String atelier) {
-		this.atelier = atelier;
+	public void setAtelier(String sAtelier) {
+		this.sAtelier = sAtelier;
 	}
 
 	public int[][] getTempsTransfertParcelle() {
-		return tempsTransfertParcelle;
+		return iTTempsTransfertParcelle;
 	}
 
-	public void setTempsTransfertParcelle(int[][] tempsTransfertParcelle) {
-		this.tempsTransfertParcelle = tempsTransfertParcelle;
+	public void setTempsTransfertParcelle(int[][] iTTempsTransfertParcelle) {
+		this.iTTempsTransfertParcelle = iTTempsTransfertParcelle;
 	}
 
 
